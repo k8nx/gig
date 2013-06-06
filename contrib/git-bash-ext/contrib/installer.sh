f8c9db2e17483364eeb48397a8c9e72d94aa9146
Daegeun Kim <dgkim84@gmail.com>
2013-06-06 23:16:35 +0900

    A

--- contrib/git-bash-ext/contrib/installer.sh
+++ contrib/git-bash-ext/contrib/installer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 INSTALL_PREFIX=${INSTALL_PREFIX-"/usr/local/bin"}
 REPO_HOME=${REPO_HOME-"https://github.com/dgkim84/gig"}
+GIG_PATH="/usr/local/lib/gig"
 BRANCH=${BRANCH-"master"}
-NAME="git-ext-ignore"
 MAINFILE="git-ignore"
 SCRIPTS=("git-ignore-env" \
   "git-ignore-init" \
@@ -11,14 +11,14 @@ SCRIPTS=("git-ignore-env" \
   "git-ignore-update" \
 )
 
-if [ -d "$NAME" -a -d "$NAME/.git" ]; then
-  echo "exist repository"
+if [ -d "$GIG_PATH" -a -d "$GIG_PATH/.git" ]; then
+  echo # pass
 else
-  git clone $REPO_HOME $NAME --branch $BRANCH
+  git clone $REPO_HOME $GIG_PATH --branch $BRANCH
 fi
 
 install -v -d -m 0755 "$INSTALL_PREFIX"
-install -v -m 0755 "$NAME/contrib/git-bash-ext/$MAINFILE" "$INSTALL_PREFIX"
+install -v -m 0755 "$GIG_PATH/contrib/git-bash-ext/$MAINFILE" "$INSTALL_PREFIX"
 for SCRIPT in ${SCRIPTS[@]}; do
-  install -v -m 0644 "$NAME/contrib/git-bash-ext/$SCRIPT" "$INSTALL_PREFIX"
+  install -v -m 0644 "$GIG_PATH/contrib/git-bash-ext/$SCRIPT" "$INSTALL_PREFIX"
 done
